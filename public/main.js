2edd3b7c3bf37c75b46f1c3f16ed60f45073b416
Minjae Chang <minjaejchang@gmail.com>
2021-11-25 10:35:49 +1300

    Fix the  navbar scrolling

--- public/main.js
+++ public/main.js
@@ -1,8 +1,23 @@
-// toggle button
+// Navbar
 
-const toggleBtn = document.querySelector('.navbar__toggle-btn');
-const navbarMenu = document.querySelector('.navbar__menu');
+// Handle scrolling when tapping on the navbar item
 const navbarItem = document.querySelectorAll('.navbar__item');
+const navbarMenu = document.querySelector('.navbar__menu');
+
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+
+  if (link === null) {
+    return;
+  }
+
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: 'smooth', block: 'start' });
+});
+
+// toggle button
+const toggleBtn = document.querySelector('.navbar__toggle-btn');
 
 toggleBtn.addEventListener('click', () => {
   navbarMenu.classList.toggle('open');
